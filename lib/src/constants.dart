2ba65c50b5c62b6b0b60dfe2f3f98e691ad9ec3f
João Sereia <joao.lo.sereia@gmail.com>
2025-11-22 17:37:39 -0300

    feat(constants): add build time constants

---
+++ lib/src/constants.dart
@@ -0,0 +1,10 @@
+abstract class Constants {
+  static const String author = .fromEnvironment('AUTHOR');
+  static const String license = .fromEnvironment('LICENSE');
+  static const String package = .fromEnvironment('PACKAGE');
+  static const String version = .fromEnvironment('VERSION');
+  static const String website = .fromEnvironment('WEBSITE');
+  static const String business = .fromEnvironment('BUSINESS');
+  static const String repository = .fromEnvironment('REPOSITORY');
+  static const String description = .fromEnvironment('DESCRIPTION');
+}
